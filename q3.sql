bccbbb8194fc9eb621b3fc81356557b070d39266
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-05-10 21:26:25 +0300

    changed names

--- q3.sql
+++ q3.sql
@@ -1,6 +1,6 @@
 
-SELECT name as member_name, 
-FROM memberInKnesset natural join members 
-group by uid, name
-having count(distinct party) = 1 and count(distinct knesset_number) >= 5
-order by member_name;
+SELECT name
+FROM memberInKnesset NATURAL JOIN members 
+GROUP BY uid, name
+HAVING count(DISTINCT party) = 1 and count(DISTINCT number) >= 5
+ORDER BY name;
--- q4.sql
+++ q4.sql
@@ -1,15 +1,15 @@
 
-WITH party_size AS (
+WITH Parties AS (
     SELECT number, party, COUNT(DISTINCT uid) AS party_size
     FROM memberInKnesset
     GROUP BY number, party
 )
 
 SELECT p1.number, p1.party, p1.party_size
-FROM party_size p1
+FROM Parties p1
 WHERE p1.party_size >= ALL (
     SELECT p2.party_size
-    FROM party_size p2
+    FROM Parties p2
     WHERE p1.number = p2.number
 )
 ORDER BY p1.number, p1.party;
