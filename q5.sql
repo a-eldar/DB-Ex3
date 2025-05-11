2f18b99e3835f3236edd05d1555ae09316b79973
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-05-11 15:10:59 +0300

    fixed q5 integer division

--- q5.sql
+++ q5.sql
@@ -2,10 +2,10 @@ WITH PartyWomen(party, number, femaleCount) AS (
     SELECT party, number, count(*) AS femaleCount
     FROM memberInKnesset NATURAL JOIN members
     WHERE gender = 'female'
-    GROUP BY party, number;
+    GROUP BY party, number
 )
-SELECT party, number, femaleCount / count(*) * 100
+SELECT party, number, femaleCount * 1.0 / count(*) * 100 AS femalePercent
 FROM memberInKnesset NATURAL JOIN PartyWomen
 GROUP BY party, number, femaleCount
-HAVING femaleCount / count(*) >= 0.3
+HAVING femaleCount * 1.0 / count(*) >= 0.3
 ORDER BY party, number;
